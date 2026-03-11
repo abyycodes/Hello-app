0001d43b636ba7acdbe2d7d6aa0be336e78abaea
Abhimanyu <ab7905@srmist.edu.in>
2026-03-12 02:07:16 +0530

    Initial commit

---
+++ OOPSBannerAp/UC5.java
@@ -0,0 +1,21 @@
+public class UC5 {
+
+    public static void main(String[] args) {
+
+        // Define and populate the String array using inline initialization
+        String[] lines = {
+            String.join(" ", " ** ", " ** ", " ** ", " ** "),
+            String.join(" ", "*     ", "     ", "     ", "     *"),
+            String.join(" ", "*     ", "     ", "     ", "     *"),
+            String.join(" ", "*     ", "     ", "** ", " *** "),
+            String.join(" ", "*     ", "     ", "      ", "      *"),
+            String.join(" ", "*     ", "     ", "      ", "*     *"),
+            String.join(" ", " ** ", " ** ", "*      ", " *** ")
+        };
+
+        // Use for-each loop to print each line
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
