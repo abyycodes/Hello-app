74a3e89d3771fe19975d4488fb73701673a029b9
Abhimanyu <ab7905@srmist.edu.in>
2026-03-31 23:46:32 +0530

    Initial Commit

---
+++ OOPSBannerAp/hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Please provide a name as a command line argument.");
+        }
+    }
+}
